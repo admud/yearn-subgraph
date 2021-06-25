e713def0e4c82914677567b696f325556ef1fcd9
Phyllice Wai <waikahkwan0903@gmail.com>
2021-06-25 15:51:49 +0800

    Add DAO Faang Stonk.

--- src/utils/helpers/yearn-farmer/vault.ts
+++ src/utils/helpers/yearn-farmer/vault.ts
@@ -19,7 +19,7 @@ import { YearnFighter } from "../../../../generated/YearnFighterUSDT/YearnFighte
 import { CompoundFighter } from "../../../../generated/CompoundFighterUSDT/CompoundFighter";
 import { HarvestFighter } from "../../../../generated/HarvestFighterUSDT/HarvestFighter";
 import { Citadel } from "../../../../generated/DAOVaultCitadel/Citadel";
-
+import { FaangStonk } from "../../../../generated/DAOVaultFaangStonk/FaangStonk";
 // Strategies
 import { YearnFarmerv2 } from "../../../../generated/YearnFighterUSDT/YearnFarmerv2";
 import { CompoundFarmer } from "../../../../generated/CompoundFighterUSDT/CompoundFarmer";
@@ -325,7 +325,7 @@ export function getOrCreateCitadelFarmer(
       // Might be worth using the "try_" version of these calls in the future.
       // let underlyingTokenAddress = vaultContract.token();
       // let underlyingToken = getOrCreateToken(underlyingTokenAddress);
-      
+
       // Get ETH Price from External Contract
       let proxyContract = EACAggregatorProxy.bind(Address.fromString("0x0bF499444525a23E7Bb61997539725cA2e928138"));
       let ethPrice = proxyContract.latestAnswer();
@@ -349,6 +349,74 @@ export function getOrCreateCitadelFarmer(
     }
   }
 
+  return vault as Farmer;
+}
+
+export function getOrCreateFaangFarmer(
+  vaultAddress: Address,
+  update: boolean = true
+): Farmer {
+  let vault = Farmer.load(vaultAddress.toHexString());
+  let vaultContract = FaangStonk.bind(vaultAddress);
+
+  if (vault == null) {
+    vault = new Farmer(vaultAddress.toHexString());
+
+    // Initialize parsed values as BigDecimal 0
+    vault.earnPricePerFullShare = BIGDECIMAL_ZERO;
+    vault.vaultPricePerFullShare = BIGDECIMAL_ZERO;
+    vault.netDeposits = BIGDECIMAL_ZERO;
+    vault.totalDeposited = BIGDECIMAL_ZERO;
+    vault.totalWithdrawn = BIGDECIMAL_ZERO;
+    vault.totalActiveShares = BIGDECIMAL_ZERO;
+    vault.totalSharesMinted = BIGDECIMAL_ZERO;
+    vault.totalSharesBurned = BIGDECIMAL_ZERO;
+    vault.earnBalance = BIGDECIMAL_ZERO;
+    vault.vaultBalance = BIGDECIMAL_ZERO;
+    vault.totalSupply = BIGDECIMAL_ZERO;
+    vault.totalEarnings = BIGDECIMAL_ZERO;
+    vault.pool = BIGDECIMAL_ZERO;
+    vault.earnBalance = BIGDECIMAL_ZERO;
+    vault.vaultBalance = BIGDECIMAL_ZERO;
+
+    // Initialize raw values as BigInt 0
+    vault.netDepositsRaw = BIGINT_ZERO;
+    vault.totalDepositedRaw = BIGINT_ZERO;
+    vault.totalWithdrawnRaw = BIGINT_ZERO;
+    vault.totalActiveSharesRaw = BIGINT_ZERO;
+    vault.totalSharesMintedRaw = BIGINT_ZERO;
+    vault.totalSharesBurnedRaw = BIGINT_ZERO;
+    vault.vaultBalanceRaw = BIGINT_ZERO;
+    vault.totalSupplyRaw = BIGINT_ZERO;
+    vault.totalEarningsRaw = BIGINT_ZERO;
+    vault.earnPricePerFullShareRaw = BIGINT_ZERO;
+    vault.vaultPricePerFullShareRaw = BIGINT_ZERO;
+    vault.poolRaw = BIGINT_ZERO;
+    vault.earnBalanceRaw = BIGINT_ZERO;
+    vault.vaultBalanceRaw = BIGINT_ZERO;
+  }
+
+  if(update) {
+    let strategyAddress = vaultContract.try_strategy();
+    if (!strategyAddress.reverted) {
+      // The vault itself is an ERC20
+      let shareToken = getOrCreateToken(vaultAddress);
+
+      let totalSupply = vaultContract.try_totalSupply();
+      vault.poolRaw = vaultContract.getTotalValueInPool();
+      vault.totalSupplyRaw = !totalSupply.reverted
+        ? totalSupply.value
+        : vault.totalSupplyRaw;
+      // vault.underlyingToken = underlyingToken.id;
+      vault.shareToken = shareToken.id;
+
+      vault.totalSupply = toDecimal(
+        vault.totalSupplyRaw,
+        vaultContract.decimals()
+      );
+      vault.pool = toDecimal(vault.poolRaw, vaultContract.decimals());
+    }
+  }
   return vault as Farmer;
 }
 
